4f9b5ff90c09dbd927df1a15ddaa811361bc07d2
Sebastian Russo <russo.sebastian@gmail.com>
2021-02-15 13:48:49 -0500

    added css styling, refactored day component to hourly, made helper function

--- src/components/day.jsx
+++ src/components/day.jsx
@@ -10,11 +10,10 @@ export const Hourly = ({day, items}) => {
   // find date of selected day, match to list of 40 dates, use that index for the start of hourly forecast 
   const date = day[0].dt;
   const listOfDates = items.list.map((x,i) => x.dt);
-  let indexStart = listOfDates.findIndex(d => d === date)
-  console.log(indexStart)
+  let indexStart = listOfDates.findIndex(d => d === date);
 
   // indexStart begins index, add 5 consecutive numbers to it
-  let hourlyArray = []
+  let hourlyArray = [];
   let length = indexStart + 5;
   for (let i=indexStart; i< length; i++) { 
     hourlyArray.push(indexStart)
@@ -26,6 +25,7 @@ export const Hourly = ({day, items}) => {
   const hourlyDay = hourly.map((item,i) => {
     let date = new Date(item.dt * 1000);
     let day = date.toString().slice(0,3);
+    let time = date.toString().slice(15, 21)
     let forecast1 = item.weather[0].main; // Clouds/Snow/Rain/Clear/Thunderstorm
     let pictureForecast = imageForecast(forecast1)
     let tempHigh = `High: ${Math.round(((item.main.temp_max - 273.15) * 9/5 + 32)*10)/10}`;
@@ -33,7 +33,7 @@ export const Hourly = ({day, items}) => {
     return (
       <div  key={i} >
         <div className="wrapper-hourly">
-              <div className="box-1">{day}</div>
+              <div className="box-1">{day} {time}</div>
               <div className="box-3">{tempHigh}</div>
               <div className="box-4">{tempLow}</div>
               <img src={pictureForecast} alt="weather" width="100%"/>
